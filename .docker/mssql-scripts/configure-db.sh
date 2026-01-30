8f570e912e09aadb3c11c4d43168da47b59abc87
anhvlt-2k6 <anhvlt.2006@outlook.com>
2026-01-31 01:19:19 +0700

    fix for improper connection to sqlcmd

--- .docker/mssql-scripts/configure-db.sh
+++ .docker/mssql-scripts/configure-db.sh
@@ -3,13 +3,15 @@
 set -euo pipefail
 
 DBSTATUS=1
+ERRCODE=1
 i=0
 
 echo "Starting the custom SQL setup..."
 
-while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]]; do
+while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
 	i=$i+1
-	DBSTATUS=$(/opt/mssql-tools18/bin/sqlcmd -h -1 -t 1 -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
+	DBSTATUS=$(/opt/mssql-tools18/bin/sqlcmd -h -1 -t 1 -S 127.0.0.1 -U sa -P $MSSQL_SA_PASSWORD -C -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
+	ERRCODE=$?
 	echo "Still starting up MSSQL. Counting: $i"
 	sleep 1
 done
@@ -21,6 +23,6 @@ fi
 
 # Run the setup script to create the DB and the schema in the DB
 echo "Importing custom database"
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -d master -i /usr/config/setup.sql
+/opt/mssql-tools18/bin/sqlcmd -S 127.0.0.1 -U sa -P $MSSQL_SA_PASSWORD -C -d master -i /usr/config/setup.sql
 
 echo "Custom SQL setup is done, you now can use the database"
