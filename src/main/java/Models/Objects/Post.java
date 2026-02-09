db1db8501c24aadfc24202364d8ee77f60076135
nguyenduyk19 <thienku74@gmail.com>
2026-02-09 17:24:11 +0700

    Add model
    Signed-off-by: nguyenduyk19 <thienku74@gmail.com>

---
+++ src/main/java/Models/Objects/Post.java
@@ -0,0 +1,27 @@
+package Models.Objects;
+
+public class Post {
+
+    private int id;
+    private int userId;
+    private int categoryId;
+    private String title;
+    private String content;
+    private boolean isHidden;
+
+    public Post(int id, int userId, int categoryId, String title, String content, boolean isHidden) {
+        this.id = id;
+        this.userId = userId;
+        this.categoryId = categoryId;
+        this.title = title;
+        this.content = content;
+        this.isHidden = isHidden;
+    }
+
+    public int getId() { return id; }
+    public int getUserId() { return userId; }
+    public int getCategoryId() { return categoryId; }
+    public String getTitle() { return title; }
+    public String getContent() { return content; }
+    public boolean isHidden() { return isHidden; }
+}
