5b281b63d72a6e32c12a18d599ed429cc422a9fe
nguyenduyk19 <thienku74@gmail.com>
2026-02-10 17:06:47 +0700

    Update Post.java
    Signed-off-by: nguyenduyk19 <thienku74@gmail.com>

--- src/main/java/Models/Objects/Post.java
+++ src/main/java/Models/Objects/Post.java
@@ -9,7 +9,13 @@ public class Post {
     private String content;
     private boolean isHidden;
 
-    public Post(int id, int userId, int categoryId, String title, String content, boolean isHidden) {
+  
+    public Post() {
+    }
+
+    // constructor 
+    public Post(int id, int userId, int categoryId,
+                String title, String content, boolean isHidden) {
         this.id = id;
         this.userId = userId;
         this.categoryId = categoryId;
@@ -18,10 +24,32 @@ public class Post {
         this.isHidden = isHidden;
     }
 
+    // getters
     public int getId() { return id; }
     public int getUserId() { return userId; }
     public int getCategoryId() { return categoryId; }
     public String getTitle() { return title; }
     public String getContent() { return content; }
     public boolean isHidden() { return isHidden; }
+
+ 
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setHidden(boolean hidden) {
+        isHidden = hidden;
+    }
 }
