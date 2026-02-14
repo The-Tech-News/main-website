6be9528c389aa2242d435ac8c2f7aed2ce36236c
anhvlttfs <anhvlt.2006@outlook.com>
2026-02-14 09:53:25 +0700

    Create CategoryDAO.java

---
+++ src/main/java/Models/DAO/CategoryDAO.java
@@ -0,0 +1,87 @@
+package Models.DAO;
+
+import Models.DBContext;
+import Models.Objects.Category;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CategoryDAO extends DBContext {
+
+    public ArrayList<Category> GetListCategory() {
+        ArrayList<Category> catList = new ArrayList<>();
+
+        String sqlCommand = """
+                            SELECT [id], [name], [description]
+                                FROM [technewsdb].[dbo].[Category];
+                            """;
+
+        try (PreparedStatement ps = super.getConnection().prepareStatement(sqlCommand); ResultSet rs = ps.executeQuery()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            String description = rs.getString("description");
+            catList.add(new Category(id, name, description));
+        } catch (SQLException sqlEx) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, sqlEx);
+        }
+
+        return catList;
+    }
+
+    public int NewCategory(String name, String description) {
+        int returnValue = -1;
+
+        String sqlCommand = """
+                            DECLARE	@return_value int;
+                            EXEC	@return_value = [technewsdb].[dbo].[NewCategory]
+                                        @name = ?,
+                                        @description = ?;
+                            SELECT	'retval' = @return_value;
+                            """;
+
+        try (PreparedStatement ps = super.getConnection().prepareStatement(sqlCommand)) {
+            ps.setString(1, name);
+            ps.setString(2, description);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    returnValue = rs.getInt("retval");
+                }
+            }
+        } catch (SQLException sqlEx) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, sqlEx);
+        }
+
+        return returnValue;
+    }
+
+    public int EditCategory(String oldName, String newName, String description) {
+        int returnValue = -1;
+
+        String sqlCommand = """
+                            DECLARE	@return_value int;
+                            EXEC	@return_value = [technewsdb].[dbo].[EditCategory]
+                                        @oldName = ?,
+                                        @newName = ?
+                                        @description = ?;
+                            SELECT	'retval' = @return_value;
+                            """;
+
+        try (PreparedStatement ps = super.getConnection().prepareStatement(sqlCommand)) {
+            ps.setString(1, oldName);
+            ps.setString(2, newName);
+            ps.setString(3, description);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    returnValue = rs.getInt("retval");
+                }
+            }
+        } catch (SQLException sqlEx) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, sqlEx);
+        }
+
+        return returnValue;
+    }
+}
