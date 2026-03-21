1bea717f9dd78a17672d8b351f4a8d19d59b7bc1
anhvlttfs <anhvlt.2006@outlook.com>
2026-03-21 10:57:21 +0700

    Update Index.java

--- src/main/java/Controllers/Index.java
+++ src/main/java/Controllers/Index.java
@@ -16,9 +16,4 @@ public class Index extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/JSPViews/index.jsp").forward(request, response);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/JSPViews/index.jsp").forward(request, response);
-    }
 }
