5d84c881620728caa446ca54e618e06ba08a1a47
anhvlttfs <anhvlt.2006@outlook.com>
2026-03-21 13:24:55 +0700

    Update Index.java

--- src/main/java/Controllers/Index.java
+++ src/main/java/Controllers/Index.java
@@ -1,6 +1,8 @@
 package Controllers;
 
+import Models.DAO.CategoryDAO;
 import Models.DAO.PostDAO;
+import Models.DAO.UserDAO;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,14 +14,22 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Index extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    
     private final PostDAO postDao;
+    private final CategoryDAO categoryDao;
+    private final UserDAO userDAO;
 
     public Index() {
         this.postDao = new PostDAO();
+        this.categoryDao = new CategoryDAO();
+        this.userDAO = new UserDAO();
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("category", this.categoryDao.GetHashCategory());
+        request.setAttribute("post", this.postDao.GetHomePostTop());
+        request.setAttribute("users", this.userDAO.GetUserName());
         request.getRequestDispatcher("/WEB-INF/JSPViews/index.jsp").forward(request, response);
     }
 }
