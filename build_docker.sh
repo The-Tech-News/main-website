ffeca5f5bbf7df0dc037e37d7240bae578601cb7
anhvlttfs <anhvlt.2006@outlook.com>
2026-03-23 10:18:53 +0700

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 cd "$(cd "$(dirname "$0")" && pwd)"
 
-docker build -f ./.docker/dockerfile-web -t "anhvlt/the-tech-news:web-latest" .
-docker build -f ./.docker/dockerfile-db -t "anhvlt/the-tech-news:db-latest" .
+docker build -f ./.docker/dockerfile-web -t "anhvlttfs/the-tech-news:web-latest" .
+docker build -f ./.docker/dockerfile-db -t "anhvlttfs/the-tech-news:db-latest" .
